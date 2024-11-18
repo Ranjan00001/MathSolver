94e61a3e1a76fa55764d52524b670ee300083d7c
Deependra Shukla <sdeependrashukla093@gmail.com>
2024-11-18 16:51:44 +0530

    Mapped Train images to their text in csv file.

--- compare_model/main.py
+++ compare_model/main.py
@@ -1,9 +1,10 @@
 from ocr import OCR
 
-ocr=OCR(image_folder=r"C:\Users\Deependra\Desktop\DeepLearning\Cnn\cv project\compare_model\test")
+ocr=OCR(image_folder=r"C:\Users\Deependra\Desktop\DeepLearning\Cnn\cv project\MathSolver\compare_model\test")
 
 
 if __name__ == "__main__":
-    ocr.keras_ocr_works()
+    print("hello world")
+    # ocr.keras_ocr_works()
     ocr.easyocr_model_works()
-    ocr.pytesseract_model_works()
+    # ocr.pytesseract_model_works()
